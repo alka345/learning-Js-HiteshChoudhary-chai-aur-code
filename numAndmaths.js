50360006d4efd5df64f6cf9e8e0b6356fdbf9574
alka345 <alkaraojnv@gmail.com>
2023-11-24 12:56:31 +0530

    learn number properties and maths operations

---
+++ numAndmaths.js
@@ -0,0 +1,42 @@
+let score =40;
+// console.log(score);
+let balance = new Number(400);
+// console.log(balance);
+
+let price = 8.593;
+//round off the number
+// console.log(price.toFixed());
+let budget = 100000;
+// console.log(budget.toLocaleString( ));//100,000
+// console.log(budget.toLocaleString('en-In'));//1,00,000
+
+let scoreString= console.log(score.toString());
+// console.log(typeof scoreString);//converted to string 
+//when number converted to string different properties of string can be use here
+
+// console.log(Math.abs(-4));//4 it converts -ve to +ve.Not vice versa true.
+// console.log(Math.abs(4));//4
+// console.log(Math.round(4.7));//round off number to 5
+// console.log(Math.ceil(4.2)); //5
+// console.log(Math.floor(4.9));//4
+// console.log(Math.sqrt(25));//5
+// console.log(Math.pow(8,2));//64
+// console.log(Math.min());//Infinity
+// console.log(Math.max());//-Infinity
+
+// console.log(Math.random());//any random number
+// console.log((Math.random()*10)+1);//number = or > than 1
+// console.log(Math.floor(Math.random()*10)+1);
+// let min=30; let max=40;
+// console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+//node numAndmaths.js
