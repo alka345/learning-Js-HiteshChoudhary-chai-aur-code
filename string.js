e9dce2222d5a92cbbb7f6a6ac1377aa71656c5ac
alka345 <alkaraojnv@gmail.com>
2023-11-24 10:16:28 +0530

    Learn string and its methods

---
+++ string.js
@@ -0,0 +1,15 @@
+const myName ="Alka-Rao-Learning-js"
+const repoCount = 90;
+// console.log(name +repoCount+"Values");//not recommended to use 
+
+// console.log(`hello,My repocount is${repoCount}`);//instead use this-string interpolation
+
+console.log(myName[5]);//R
+ console.log(myName.__proto__);//{ }
+ console.log(myName.length);//20
+//  console.log(myName.toUpperCase());ALKA-RAO_LEARNING_JS-ERROR
+ console.log(myName.charAt(5));//R
+ console.log(myName.indexOf('R'));//5
+ console.log(myName.substring(0,4));//Alka
+ console.log(myName.slice(0,4));//Alka
+
