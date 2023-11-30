c15f4163fd5471d1d69b21fbccaef3b58ef699e7
alka345 <alkaraojnv@gmail.com>
2023-11-30 13:07:25 +0530

    learn about scope

---
+++ scope.js
@@ -0,0 +1,14 @@
+// var c=400;
+// let a=300;
+// if (true){
+//     // let a=10;
+//     const b=20;
+//     console.log("INNER:",a);
+// }
+
+function one(){
+    const username ="Alka"
+    function two(){
+        
+    }
+}
