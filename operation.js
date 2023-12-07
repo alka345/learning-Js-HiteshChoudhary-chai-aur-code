5cc1f44e74dde0e8863ab72a7175ecc6c830a417
alka345 <alkaraojnv@gmail.com>
2023-12-07 09:25:10 +0530

    code review

--- operation.js
+++ operation.js
@@ -7,8 +7,11 @@
 // console.log(true+);//error
 
 // console.log(+"");//0
- let num1, num2,num3;
- num1=num2=num3=9+1;//wrong way of writing code
+//  let num1, num2,num3;
+//  num1=num2=num3=9+1;//wrong way of writing code
 //  precedence operator
 // prefix
 // postfix
+
+console.log("2"===2);
+console.log("2"==2);
